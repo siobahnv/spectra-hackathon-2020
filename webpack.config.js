8b3d5b018af9f0cd9ed4aed4bc2c98fbc75e3c35
Heather Ransome <siobahnv@gmail.com>
2020-08-08 16:26:10 -0700

    Messing with devserver in webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
@@ -40,5 +41,13 @@ module.exports = {
       template: "./src/index.html",
       filename: "./index.html"
     })
-  ]
+  ],
+  devServer: {
+    contentBase: [path.join(__dirname, 'build'), path.join(__dirname, 'public')],
+    allowedHosts: [
+      'glitch.com',
+      'glitch.me',
+      'siobahnv-spectra-hackathon-2020.glitch.me'
+    ]
+  }
 };
