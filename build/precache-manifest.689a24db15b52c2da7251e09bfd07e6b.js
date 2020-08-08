f6287a04495555ecafdd269d4d938cfb46df4eac
Heather Ransome <siobahnv@gmail.com>
2020-08-08 13:40:20 -0700

    Restructured files to top level.

---
+++ build/precache-manifest.689a24db15b52c2da7251e09bfd07e6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43ced786fd4884f94094b48b61763ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2acca57bed47794b6def",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f86508cb417b5960356a",
+    "url": "/static/js/2.b673a62a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b673a62a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2acca57bed47794b6def",
+    "url": "/static/js/main.93a92654.chunk.js"
+  },
+  {
+    "revision": "2f6b803df0c99d778b98",
+    "url": "/static/js/runtime-main.584d0ba3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
